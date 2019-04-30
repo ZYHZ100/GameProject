0126dd2f7fc63f34a6788f832a3723da4052173e
tpsoete <tpsoete@163.com>
2019-04-30 19:45:46 +0800

    initial commit

---
+++ Source/GameProject/GameProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGameProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGameProjectGameMode();
+};
+
+
+
