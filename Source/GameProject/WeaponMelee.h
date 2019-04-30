a08bb1cb6a34828e10c2b3281ad3313282b9b243
tpsoete <tpsoete@163.com>
2019-04-30 23:36:32 +0800

    add empty classes

---
+++ Source/GameProject/WeaponMelee.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon.h"
+#include "WeaponMelee.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEPROJECT_API AWeaponMelee : public AWeapon
+{
+	GENERATED_BODY()
+	
+};
